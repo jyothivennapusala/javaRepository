0175f247986572350af50ba5dddba0522ccc93a3
jyothi <jyothi.vennapusala@gmail.com>
2022-05-19 16:23:25 +0530

    new comit for modification....

---
+++ GITproject/src/NewStringModifiedClass.java
@@ -0,0 +1,12 @@
+
+public class NewStringModifiedClass {
+
+
+	public static void main(String[] args) throws InterruptedException {
+		String a="java";
+		String b="Programming";
+		
+		System.out.println(a+b);
+}
+}
+
