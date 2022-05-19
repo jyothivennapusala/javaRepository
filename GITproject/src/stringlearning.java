72aeeb516d3973b564812d0c4bd5c6f47fb894ac
jyothi <jyothi.vennapusala@gmail.com>
2022-05-19 15:42:39 +0530

    newnew commit

---
+++ GITproject/src/stringlearning.java
@@ -0,0 +1,7 @@
+public class stringlearning {
+	public static void main(String[] args) throws InterruptedException {
+		String a="java";
+		String b="Programming";
+		System.out.println(a+"    "+b);
+	}
+}
