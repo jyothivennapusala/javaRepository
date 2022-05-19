cf663ef3669afd471a3890c7074390439158cd82
jyothi <jyothi.vennapusala@gmail.com>
2022-05-19 15:11:03 +0530

    Java Commit.....

---
+++ GITproject/src/stringsLearning.java
@@ -0,0 +1,11 @@
+
+
+public class stringsLearning {
+	public static void main(String[] args) throws InterruptedException {
+		String a="java";
+		String b="Programming";
+		String c=a.concat(b);
+		System.out.println(c);
+		System.out.println(a+b);
+}
+}
